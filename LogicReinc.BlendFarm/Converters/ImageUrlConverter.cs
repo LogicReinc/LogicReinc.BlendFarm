640cfcf8ce8b1515fe7fc49b75b7a8fcb0f9424d
Kelvin <logicincarnate@gmail.com>
2021-10-25 22:40:19 +0200

    Announcement system, release candidate

---
+++ LogicReinc.BlendFarm/Converters/ImageUrlConverter.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicReinc.BlendFarm.Converters
+{
+    public class ImageUrlConverter : IValueConverter
+    {
+        private static Dictionary<string, Bitmap> _cache = new Dictionary<string, Bitmap>();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string url = value as string;
+
+            if (_cache.ContainsKey(url))
+                return _cache[url];
+
+            using (WebClient client = new WebClient())
+            using (MemoryStream stream = new MemoryStream(client.DownloadData(url)))
+            {
+                Bitmap bitmap = new Bitmap(stream);
+                _cache.Add(url, bitmap);
+            }
+            return _cache[url];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
