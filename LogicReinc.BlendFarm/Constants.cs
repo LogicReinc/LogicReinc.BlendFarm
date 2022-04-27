53a80cbcd0dbc3597b079f1950d218f021d4fb3b
Kelvin <logicincarnate@gmail.com>
2022-04-27 22:39:54 +0200

    Fix url

--- LogicReinc.BlendFarm/Constants.cs
+++ LogicReinc.BlendFarm/Constants.cs
@@ -6,6 +6,6 @@ namespace LogicReinc.BlendFarm
 {
     public static class Constants
     {
-        public const string AnnouncementUrl = "https://raw.githubusercontent.com/LogicReinc/LogicReinc.BlendFarm/main/.data/announcements.json";
+        public const string AnnouncementUrl = "https://raw.githubusercontent.com/LogicReinc/LogicReinc.BlendFarm/dev-1.1.1/.data/announcements.json";
     }
 }
