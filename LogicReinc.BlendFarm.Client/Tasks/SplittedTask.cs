7ba6f01a0e1c0b0e313dc40796a83a6990108d67
Lyze <11274700+lyze237@users.noreply.github.com>
2024-05-29 18:20:47 +0200

    Fix crash on linux

--- LogicReinc.BlendFarm.Client/Tasks/SplittedTask.cs
+++ LogicReinc.BlendFarm.Client/Tasks/SplittedTask.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ImageConverter = LogicReinc.BlendFarm.Client.ImageTypes.ImageConverter;
 
 namespace LogicReinc.BlendFarm.Client.Tasks
 {
